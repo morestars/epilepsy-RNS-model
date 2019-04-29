840cb79b3905c0254e7563c69e31028620093acc
morestars <33098474+morestars@users.noreply.github.com>
2019-04-29 19:01:30 -0400

    file read statements to main

--- main.m
+++ main.m
@@ -1,5 +1,15 @@
-%[hdr, record] = edfread('chb01_01.edf');
 
+%% Load in data
+%[hdr, record] = edfread('chb01_01.edf');
+[hdr, chb03_01] = edfread('chb03_01.edf');
+[hdr, chb03_02] = edfread('chb03_02.edf');
+[hdr, chb03_03] = edfread('chb03_03.edf');
+[hdr, chb03_04] = edfread('chb03_04.edf');
+[hdr, chb03_34] = edfread('chb03_34.edf');
+[hdr, chb03_35] = edfread('chb03_35.edf');
+[hdr, chb03_36] = edfread('chb03_36.edf');
+
+%% Analyze data
 f = 256;
 dt = 1/f;
 x = 0:1/f:(1/f)*(length(record)-1);
